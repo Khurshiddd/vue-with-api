6da7a6c2059284ed5595603d556a62ee8e3c129e
Khurshiddd <marifovxurshid80@gmail.com>
2023-06-16 11:05:58 -0700

    commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="d-flex flex-column flex-md-row align-items-center py-3 mb-4 border-bottom">
+        <a class="d-flex align-items-center text-dark text-decoration-none">
+            <img src="../image/10138abac57d4a8c90198e5798c14e83.png" alt="logo" style="width: 90px; height: 60px; cursor: pointer;" @click="toHomeHandler">
+        </a>
+        
+        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+            <a>
+                <RouterLink :to="{name: 'login'}" class="me-3 py-2 text-dark text-decoration-none">Login</RouterLink>
+            </a>
+            <a>
+                <RouterLink :to="{name: 'register'}" class="me-3 py-2 text-dark text-decoration-none">Register</RouterLink>
+            </a>
+        </nav>
+    </div>
+</template>
+<script>
+export default {
+    
+    methods:{
+        toHomeHandler(){
+            return this.$router.push('/')
+        }
+    }
+}
+</script>
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import {HomeView, RegisterView, LoginView} from '@/views'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path:'/',
+      name: 'Home',
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path:'/register', 
+      name: 'register',
+      component: RegisterView
+    },
+    {
+      path:'/login',
+      name: 'login',
+      component: LoginView
+    },
   ]
 })
 
